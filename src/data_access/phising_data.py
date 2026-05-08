218b8f333b571e8bd90c25c7532640f04b4750fe
harshit11purohit <harshitpurohit953@gmail.com>
2026-05-08 15:00:05 +0530

    solving mongodb issues and updating download method file

--- src/data_access/phising_data.py
+++ src/data_access/phising_data.py
@@ -18,8 +18,7 @@ class PhisingData:
 
     def __init__(self,
                  database_name: str):
-        """
-        """
+        # setting address of database
         try:
 
             self.database_name = database_name
@@ -29,7 +28,7 @@ class PhisingData:
             raise CustomException(e, sys)
 
     def get_collection_names(self) -> List:
-
+    # conects to db and return list of items of connected db
         mongo_db_client = MongoClient(self.mongo_url)
         collection_names = mongo_db_client[self.database_name].list_collection_names()
         return collection_names
@@ -42,13 +41,15 @@ class PhisingData:
             database_name=self.database_name,
             collection_name=collection_name
         )
-        df = mongo_connection.find()
+        df = mongo_connection.find() # every single row of data to dataframe
 
         if "_id" in df.columns.to_list():
             df = df.drop(columns=["_id"])
-        df = df.replace({"na": np.nan})
+        df = df.replace({"na": np.nan}) # replaces "na" with nan(not a number)
         return df
 
+
+# Generator use to return data
     def export_collections_as_dataframe(
             self) -> pd.DataFrame:
         try:
@@ -63,7 +64,24 @@ class PhisingData:
                 df = self.get_collection_data(collection_name=collection_name)
                 yield collection_name, df
 
-
-
         except Exception as e:
             raise CustomException(e, sys)
+
+
+'''The Exact Execution Flow
+
+Initialize: Grabs the Database Name and your secret Connection URL from the system.
+
+Scan: Connects to Atlas and gets a List of every collection name (table) in that database.
+
+Loop: Iterates through every name in that list one by one.
+
+Fetch & Clean: For each name:
+
+Downloads all records into a Pandas DataFrame.
+
+Deletes the _id column.
+
+Replaces any "na" text with actual NaN values.
+
+Yield: Returns the (Collection Name, DataFrame) pair and "pauses" the code until the next part of your pipeline is ready for the next table.'''
